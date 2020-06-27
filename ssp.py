29eae50498130d0b4b27757745a8370463d7aa98
cannotsleep <britecp@gmail.com>
2020-06-28 02:04:30 +0800

    Added filters to search

--- ssp.py
+++ ssp.py
@@ -13,7 +13,9 @@ sspDir = '/home/b/projects/sari_store_prices/'
 
 path.append(sspDir + 'modules')
 
-from tagloc_generate import generate
+from tagloc_generate import generateTagLocFile
+
+generateTagLocFile(sspDir)
 
 list_excel_rows = [] #list for excel row number of displayed items
 selected_line = 0 #initialize line number of selected item
@@ -30,24 +32,52 @@ col_title = '-' * 59 + '\n' + '||' + 'ITEM'.center(46) + '||' + 'PRICE'.center(7
 
 
 def updateText():
+    #check if filters are applied
+    filter_tag = False
+    filter_loc = False
+
+    selected_tag = tagsVar.get()
+    if selected_tag != 'Tags':
+        filter_tag = True
+
+    selected_loc = locVar.get()
+    if selected_loc != 'Locations':
+        filter_loc = True
+
+    #used to show all match to filters if entry is empty
+    typed = ent.get()
+    if filter_tag or filter_loc:
+        if typed == '':
+            typed = '.'
+
+    #find all match to filters and entry
     list_text = ''
     global list_excel_rows
     list_excel_rows = []
-    if ent.get() != '':
-        regex = re.compile(ent.get(), re.I) #set regex to what's typewritten in ent
+    if typed != '':
+        regex = re.compile(typed, re.I)
         for i in range(1, num_items):
-            if regex.search(str(sheet1['A' + str(i+1)].value)) != None: #append regex matches to list_text
+            regex_matched = False
+            if regex.search(str(sheet1['A' + str(i+1)].value)) != None:
+                regex_matched = True
+            if filter_tag:
+                if str(sheet1['C' + str(i+1)].value) != selected_tag:
+                    regex_matched = False
+            if filter_loc:
+                if str(sheet1['D' + str(i+1)].value) != selected_loc:
+                    regex_matched = False
+            if regex_matched:
                 list_excel_rows.append(str(i+1))
                 list_text += '||' + str(sheet1['A' + str(i+1)].value).center(46) + '||' + str(sheet1['B' + str(i+1)].value).center(7) + '||\n'
 
-    #update tkinter price_list text box
+    #update tkinter price_list text box based on matches
     price_list['state'] = 'normal'
     price_list.delete(1.0, 'end')
     price_list.insert(1.0, col_title + list_text)
     price_list['state'] = 'disabled'
 
 
-def editExcel(itemEntry, priceEntry, editWindow):
+def editItem(itemEntry, priceEntry, editWindow):
     #setting default
     updateBool = True
     iE_bg = 'white'
@@ -101,11 +131,12 @@ def editExcel(itemEntry, priceEntry, editWindow):
             sheet1['D' + list_excel_rows[selected_line - 4]].value = location
             excel_file.save(sspDir + 'ssp.xlsx')
             editWindowWithdraw()
-            updateText()
 
             #generate tag and location text files only if they're updated
             if selected_row[2] != tag or selected_row[3] != location:
-                generate(sspDir)
+                generateTagLocFile(sspDir)
+
+            updateAll()
 
             #track changes
             with open(sspDir + 'text_files/change_tracker.txt', 'a') as ct:
@@ -113,6 +144,7 @@ def editExcel(itemEntry, priceEntry, editWindow):
 
 
 def editWindowWithdraw():
+    ent.focus_set()
     editWindow.withdraw()
     ent['state'] = 'normal'
     itemEntry['bg'] = 'white'
@@ -120,16 +152,42 @@ def editWindowWithdraw():
     style.theme_use('white')
 
 
+def fetchTags():
+    tags = []
+    for tag in open(sspDir + 'text_files/tags.txt', 'r'):
+        tag = tag[:-1]
+        tags.append(tag)
+    return tags
+
+def fetchLocs():
+    locations = []
+    for location in open(sspDir + 'text_files/locations.txt', 'r'):
+        location = location[:-1]
+        locations.append(location)
+    return locations
+
+
+def updateAll():
+    global tags
+    tags = fetchTags()
+    global locations
+    locations = fetchLocs()
+    updateFilters()
+    updateText()
+
+
 def mapKey(event):
     global delBool
     #do if edit window or delete window is not active
     if editWindow.state() == 'withdrawn' and delBool == False:
-
         current_focus = str(root.focus_get())
 
         if current_focus == '.!frame.!entry':
+            if event.keysym == 'Escape': #clear entry and reset filters
+                tagsVar.set('Tags')
+                locVar.set('Locations')
+                ent.delete(0, 'end')
 
-            if event.keysym == 'Escape': ent.delete(0, 'end') #clear entry
             updateText()
 
         elif current_focus == '.!frame2.!text':
@@ -142,23 +200,10 @@ def mapKey(event):
 
                 ent['state'] = 'disabled'
 
-                #fetch list of tags
-                tags = []
-                for tag in open(sspDir + 'text_files/tags.txt', 'r'):
-                    tag = tag[:-1]
-                    tags.append(tag)
-                    if selected_row[2] == tag:
-                        tagCurInd = len(tags) - 1
+                tagCurInd = tags.index(selected_row[2])
+                locCurInd = locations.index(selected_row[3])
 
-                #fetch list of locations
-                locations = []
-                for location in open(sspDir + 'text_files/locations.txt', 'r'):
-                    location = location[:-1]
-                    locations.append(location)
-                    if selected_row[3] == location:
-                        locCurInd = len(locations) - 1
-
-                ##Set editWindow Fields
+                #-- Set editWindow Fields
                 tagCb['value'] = tags
                 tagCb.current(tagCurInd)
                 locationCb['value'] = locations
@@ -168,7 +213,7 @@ def mapKey(event):
                 priceEntry.delete(0, 'end')
                 itemEntry.insert(END, selected_row[0])
                 priceEntry.insert(END, selected_row[1])
-                ##
+                #--
 
                 editWindow.deiconify()
 
@@ -203,6 +248,7 @@ def delItem(event):
     ent['state'] = 'disabled'
     ans = messagebox.askyesno('Delete', f"Are you sure you want to delete {selected_row[0]}?")
     if ans == True:
+        ent.focus_set()
 
         #track deletion
         with open(sspDir + 'text_files/change_tracker.txt', 'a') as ct:
@@ -212,27 +258,55 @@ def delItem(event):
         sheet1.delete_rows(int(list_excel_rows[selected_line - 4]), 1)
         excel_file.save(sspDir + 'ssp.xlsx')
 
-        updateText()
+        generateTagLocFile(sspDir)
+        
+        updateAll()
 
-    ent['state'] = 'normal'    
+    ent.focus_set()
+    ent['state'] = 'normal'
+
+
+def setFilter(filter, value):
+    if filter == 'tag':
+        tagsVar.set(value)
+    elif filter == 'loc':
+        locVar.set(value)
+    updateText()
+
+
+def updateFilters():
+    #tag filter
+    tagsMenu.delete(0, 'end')
+    tagsMenu.add_command(label='Tags', command=lambda: setFilter('tag', 'Tags'))
+    tagsMenu.add_separator()
+    for i in tags:
+        tagsMenu.add_command(label=i, command=lambda value=i: setFilter('tag', value))
+
+    #location filter
+    locMenu.delete(0, 'end')
+    locMenu.add_command(label='Locations', command=lambda: setFilter('loc', 'Locations'))
+    locMenu.add_separator()
+    for i in locations:
+        locMenu.add_command(label=i, command=lambda value=i: setFilter('loc', value))
 
 
 
+#================================================= Create Main Window ===================================================
 root = Tk()
 root.title('Item finder')
 root.resizable(0,0) #remove maximize button
 
-# ws = root.winfo_screenwidth() #get device screeen width
+ws = root.winfo_screenwidth() #get device screeen width
 hs = root.winfo_screenheight() #get device screen height
 
-root.geometry(f'487x{hs-63}+0+0') #initialize window position
+root.geometry(f'478x{hs-63}+0+0') #initialize window position
 
-##create withdrawn editWindow
+#========================================== Create Withdrawn EditWindow =================================================
+
 editWindow = Toplevel(root)
 editWindow.title('Edit Item')
 editWindow.resizable(0,0) #remove maximize button
 
-ws = root.winfo_screenwidth() #get device screeen width
 editWindow.geometry(f'400x300+{ws//2-200}+{hs//2-200}') #initialize window position
 
 #create labelframes in editWindow
@@ -247,7 +321,7 @@ locationLF.grid(row=3, pady=1)
 
 #create submitButton in editWindow
 submitButton = Button(editWindow, text='Submit', bg='dim gray', activebackground='dim gray',\
-    command=lambda: editExcel(itemEntry, priceEntry, editWindow))
+    command=lambda: editItem(itemEntry, priceEntry, editWindow))
 submitButton.grid(row=4)
 
 #center widgets in editWindow
@@ -285,15 +359,37 @@ locationCb = ttk.Combobox(locationLF, width=37, justify=CENTER)
 locationCb.grid(row=0, padx=5, pady=5)
 
 editWindow.withdraw()
-##end of editWindow creation
 
+#====================================== Create Search & Filters =========================================================
 
-ent_frame = Frame(root)
-ent_frame.grid(row=0)
+search_frame = Frame(root)
+search_frame.grid(row=0)
 
-ent = Entry(ent_frame, bd=3, bg='white', fg='black', width=53)
-ent.grid(row=0)
+ent = Entry(search_frame, bd=3, bg='white', fg='black', width=27)
+ent.grid(row=0, column=0, padx=2)
 ent.focus_set()
+
+tags =  fetchTags()
+
+tagsVar = StringVar(root)
+tagsVar.set('Tags')
+tagsOM = OptionMenu(search_frame, tagsVar, ())
+tagsOM.configure(width=7)
+tagsOM.grid(row=0, column=1)
+tagsMenu = tagsOM['menu']
+
+locations = fetchLocs()
+
+locVar = StringVar(root)
+locVar.set('Locations')
+locationsOM = OptionMenu(search_frame, locVar, ())
+locationsOM.configure(width=7)
+locationsOM.grid(row=0, column=2)
+locMenu = locationsOM['menu']
+
+updateFilters()
+
+#================================================= Create Price List Text ===============================================
 
 text_frame = Frame(root)
 text_frame.grid(row=1)
@@ -304,8 +400,11 @@ price_list.grid(row=0, pady=10)
 price_list.insert(1.0, col_title)
 price_list['state'] = 'disabled'
 
+#======================================================= Binds ==========================================================
+
 root.bind('<KeyRelease>', mapKey)
 price_list.bind('<1>', selectItem)
 price_list.bind('<Delete>', delItem)
+
 
 root.mainloop()
